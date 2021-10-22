acaf013df077500a95c1a1a4d90b2dd31f812916
Mariama Bah <mariamabah465@gmail.com>
2021-10-22 13:17:52 +0000

    second  commit

--- JS/script.js
+++ JS/script.js
@@ -1,41 +1,43 @@
 $(document).ready(function(){
+  
+  // Get the navbar
+ /* var navbar = document.getElementById("navbar");
+  console.log(navbar);
+  
+  // Get the offset position of the navbar
+  var sticky = 50
+  
+  // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+  function myFunction() {
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("fixed-top");
+    } else {
+      navbar.classList.remove("fixed-top");
+    }
+  }*/
+  
+  // When the user scrolls the page, execute myFunction
+  window.onscroll = function() {myFunction()};
   // Add smooth scrolling to all links
   $("a").on('click', function(event) {
-
+    
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
       // Prevent default anchor click behavior
       event.preventDefault();
-
+      
       // Store hash
       var hash = this.hash;
-
+      
       // Using jQuery's animate() method to add smooth page scroll
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
       }, 500, function(){
-
+        
         // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
       });
     } // End if
   })
 });
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
-
-// Get the navbar
-var navbar = document.getElementById("navbar");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("fixed-top");
-  } else {
-    navbar.classList.remove("fixed-top");
-  }
-}
